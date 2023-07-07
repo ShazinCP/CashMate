a77fd3e8b27b88c2207061f109b04ed805799218
ShazinCp <shazinmuhammed746@gmail.com>
2023-07-07 04:36:18 -0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+ 
+
       title: 'CashMate',
       home: ScreenSplash(),
     );
